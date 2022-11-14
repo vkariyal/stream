79bf6499bc4165c8c3124344c4c16cd0b0d3bfad
vkariyal <107928494+vkariyal@users.noreply.github.com>
2022-11-14 10:50:09 +0530

    Updated
    updated

--- Streamlit_FirstExample.py
+++ Streamlit_FirstExample.py
@@ -1,16 +1,15 @@
 # streamlit_app.py
-
 import streamlit as st
-import snowflake as sf
-from snowflake import connector
-# import snowflake.connector
+# import snowflake as sf
+# from snowflake import connector
+import snowflake.connector
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 def init_connection():
-    return sf.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"]  #, client_session_keep_alive=True
     )
 
 conn = init_connection()
